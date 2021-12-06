ac6acb02ba918c7ebfb8ac73ddc7f7b4ee2bbcf6
ccpaging <ccpaging@gmail.com>
2021-12-06 17:50:09 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # config
+
+The config is go configuration module. It is:
+
+* Simple
+
+* Winbase api style, like GetProfileString, GetProfileInt ...
+
+* Stored in JSON, INI, TOML format.
+
+* Overridable by environment variable and arguments.
---
+++ filejson.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+)
+
+type jsonFile struct {
+	fileStore
+}
+
+func newJSONFile(path string) *jsonFile {
+	f := new(jsonFile)
+	f.path = path
+	return f
+}
+
+func (f *jsonFile) load() (map[string]interface{}, error) {
+	content, err := ioutil.ReadFile(f.path)
+	if err != nil {
+		return nil, err
+	}
+
+	out := make(map[string]interface{})
+	if err := json.Unmarshal(content, &out); err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+func (f *jsonFile) save(in map[string]interface{}) error {
+	if in == nil {
+		return errors.New("Config is nil")
+	}
+	content, err := json.MarshalIndent(in, "", "    ")
+	if err != nil {
+		return err
+	}
+	if err = ioutil.WriteFile(f.path, content, 0644); err != nil {
+		return err
+	}
+	return nil
+}
